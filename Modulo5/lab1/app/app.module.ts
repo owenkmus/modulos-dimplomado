dee7c138f53b4ac4f4c8545fe9e51c4fd5f76cf4
hjk <ghkd>
2016-10-28 18:53:03 -0500

    lab1

--- Modulo5/lab1/app/app.module.ts
+++ Modulo5/lab1/app/app.module.ts
@@ -1,19 +1,35 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
 import { AppComponent }   from './component/app.component';
-import { StudentDetailComponent } from './component/student-detail.component';
-import { TeacherDetailComponent } from './component/teacher-detail.component';
-import {FormsModule} from '@angular/forms';
+import { PageOneComponent } from './component/page-one.component';
+import {AppRoutingModule} from "./rounting/app-routing.module";
+import {AboutComponent} from "./component/about.component";
+import {PageTwoComponent} from "./component/page-two.component";
+import {HomeComponent} from "./component/home.component";
+import {APP_BASE_HREF} from "@angular/common";
+
 @NgModule({
-  imports:      [ 
-	  BrowserModule,
-	  FormsModule
-   ],
-  declarations: [ 
-  	AppComponent,
-	StudentDetailComponent,
-	TeacherDetailComponent
+  imports:      [
+      BrowserModule,
+      FormsModule,
+      AppRoutingModule
   ],
+  declarations: [
+      AppComponent,
+      PageOneComponent,
+      PageTwoComponent,
+      HomeComponent,
+      AboutComponent
+  ],
+  providers: [
+      {
+          provide: APP_BASE_HREF, useValue : '/'
+      }
+  ],
+
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
+
